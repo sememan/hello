a10605b477a97acdbcec6b0785bce2675bedda93
emanuele <sememan@libero.it>
2021-05-10 11:45:17 +0200

    deploy scripts

--- tests.py
+++ tests.py
@@ -1,5 +1,26 @@
-from hello import app
-with app.test_client() as c:
-    response = c.get('/')
-    assert response.data == b'Hello World!'
-    assert response.status_code == 200
+#from hello import app
+#with app.test_client() as c:
+#    response = c.get('/')
+#    assert response.data == b'Hello World!'
+#    assert response.status_code == 200
+
+
+import hello
+
+class TestHelloWorld(unittest.TestCase):
+
+    def setUp(self):
+        self.app = hello_world.app.test_client()
+        self.app.testing = True
+
+    def test_status_code(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_message(self):
+        response = self.app.get('/')
+        message = hello_world.wrap_html('Hello DockerCon 2018!')
+        self.assertEqual(response.data, message)
+
+if __name__ == '__main__':
+    unittest.main()
