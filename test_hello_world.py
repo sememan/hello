62ff31709f11468193dc0c7d4e51059d65d407c2
emanuele <sememan@libero.it>
2021-05-11 11:58:02 +0200

    test reference py

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,10 +1,3 @@
-#from hello import app
-#with app.test_client() as c:
-#    response = c.get('/')
-#    assert response.data == b'Hello World!'
-#    assert response.status_code == 200
-
-
 import hello
 import unittest
 
