af1e5c2f826588daffbc08eadc68f5ba6f5ad34a
emanuele <sememan@libero.it>
2021-05-11 12:14:29 +0200

    adjustements

--- gitPush.sh
+++ gitPush.sh
@@ -3,10 +3,10 @@
 REPO_DIR=/mnt/Volume/K2Systems/REPOSITORY/hello/
 cd $REPO_DIR
 pwd
-echo "Enter your message"
+echo "--- Enter your message"
 read message
 git add .
 git commit -m "${message}"
 git status
-echo "Pushing data to remote server!!!"
+echo "--- Pushing data to remote server!!!"
 git push -u origin master:deploy
