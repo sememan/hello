5172fb8eee9c3cd8e5add45c3f66f8ff585ce571
emanuele <sememan@libero.it>
2021-05-12 19:06:24 +0200

    deploy

--- deploy.py
+++ deploy.py
@@ -20,7 +20,7 @@ try:
 
     s = pxssh.pxssh()
 
-    s.login (hostname, username, ssh_key='/root/.ssh/id_rsa')
+    s.login (hostname, username, ssh_key='/home/pi/.ssh/id_rsa')
     # s.login (hostname, username, password)
     s.prompt() 
     print('\n # login:\n', str(s))
