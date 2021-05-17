0a650906fae5e17062b400d0cb46a8f3e3e50e6d
emanuele <sememan@libero.it>
2021-05-17 16:33:36 +0200

    test push

--- gitPush.sh
+++ gitPush.sh
@@ -3,12 +3,11 @@
 REPO_DIR=/mnt/Volume/K2Systems/REPOSITORY/hello/
 cd $REPO_DIR
 pwd
-echo "--- Enter your message"
+echo "--- Enter the commit message"
 read message
 git add .
 git commit -m "${message}"
 git status
-# echo "--- Pushing data to remote server!!!"
 echo "--- Enter the branch:"
 read branch
 git push -u origin "${branch}"
