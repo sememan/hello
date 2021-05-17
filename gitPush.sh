8d60104806a73dff97db53c78d6b08a47e01817c
emanuele <sememan@libero.it>
2021-05-17 16:35:11 +0200

    test again

--- gitPush.sh
+++ gitPush.sh
@@ -2,12 +2,16 @@
 
 REPO_DIR=/mnt/Volume/K2Systems/REPOSITORY/hello/
 cd $REPO_DIR
+
 pwd
+
 echo "--- Enter the commit message"
 read message
+
 git add .
 git commit -m "${message}"
 git status
+
 echo "--- Enter the branch:"
 read branch
 git push -u origin "${branch}"
